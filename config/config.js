babc0b18f618e4bd4b0cbe2f48c43a5d2bcc852f
Michael Downs <michael@chindowns.com>
2020-09-17 15:11:52 -0700

    updated config.js file for production

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   "production": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_UPW,
+    "password": process.env.DB_PW,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
